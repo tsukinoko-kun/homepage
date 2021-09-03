46e04535a0f20e63063aa059e98a4b2fc7dd31fe
Frank Mayer <frank.mayer1907@protonmail.com>
2021-09-03 19:57:34 +0200

    Added id find and preview ability to Router

--- src/script/index.ts
+++ src/script/index.ts
@@ -1,9 +1,12 @@
 import { DomFrame } from "@frank-mayer/photon";
 import MyRouter from "./MyRouter";
 
-new MyRouter({
-  frame: new DomFrame("#root"),
-  sitemap: new Set(["portfolio", "info", "links"]),
-  fallbackSite: "404",
-  homeSite: "portfolio",
-});
+new MyRouter(
+  {
+    frame: new DomFrame("#root"),
+    sitemap: new Set(["portfolio", "info", "links"]),
+    fallbackSite: "404",
+    homeSite: "portfolio",
+  },
+  location.hash
+);
