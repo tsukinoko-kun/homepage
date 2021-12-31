48cca0dda8de4505b87d482040d56279f6e0fb89
Frank Mayer <frank.mayer1907@protonmail.com>
2021-12-31 18:43:10 +0100

    finished

--- src/script/index.ts
+++ src/script/index.ts
@@ -3,12 +3,23 @@ import { DomFrame, Components, MultiLanguageRouter } from "@frank-mayer/photon";
 
 Components.resolveComponents();
 
-new MultiLanguageRouter({
+const router = new MultiLanguageRouter({
   languages: new Set(["de", "en"]),
   defaultLanguage: navigator.language.includes("de") ? "de" : "en",
   frame: new DomFrame("#root"),
-  sitemap: new Set(["portfolio", "info", "links"]),
+  sitemap: new Set(["home", "portfolio", "info", "links"]),
   fallbackSite: "404",
-  homeSite: "portfolio",
-  setWindowTitle: (newPage: string) => "Frank Mayer - " + capitalize(newPage),
+  homeSite: "home",
+  setWindowTitle: (newPage: string) => "Frank Mayer – " + capitalize(newPage),
+});
+
+router.addEventListener("injected", () => {
+  const mailEl = document.getElementById("mail") as HTMLAnchorElement;
+  if (mailEl) {
+    const mail = atob("bWFpbEBmcmFuay1tYXllci5pbw==");
+    mailEl.href = `mailto:${mail}`;
+    mailEl.innerText = mail;
+  }
+
+  window.scrollTo(0, 0);
 });
