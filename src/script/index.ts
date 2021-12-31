de23b31c70cc86c11ba51cc8c466a6e25732fa67
Frank Mayer <frank.mayer1907@protonmail.com>
2021-12-31 02:50:27 +0100

    start

--- src/script/index.ts
+++ src/script/index.ts
@@ -1,62 +1,14 @@
 import { capitalize } from "@frank-mayer/magic";
-import { DomFrame, Components } from "@frank-mayer/photon";
-import MyRouter from "./MyRouter";
+import { DomFrame, Components, MultiLanguageRouter } from "@frank-mayer/photon";
 
 Components.resolveComponents();
 
-new MyRouter(
-  {
-    languages: new Set(["de", "en"]),
-    defaultLanguage: "en",
-    frame: new DomFrame("#root"),
-    sitemap: new Set(["portfolio", "info", "links"]),
-    fallbackSite: "404",
-    homeSite: "portfolio",
-    setWindowTitle: (newPage: string) => "Frank Mayer - " + capitalize(newPage),
-  },
-  location.hash
-);
-
-{
-  const elo: AddEventListenerOptions = {
-    capture: false,
-    once: false,
-    passive: true,
-  };
-
-  const mousePos = [0, 0];
-
-  const setTorchPos = (pos: { clientX: number; clientY: number }) => {
-    mousePos[0] = Math.round(pos.clientX);
-    mousePos[1] = Math.round(pos.clientY);
-    updateTorchStyle();
-  };
-
-  const updateTorchStyle = () => {
-    document.body.style.setProperty("--x", `${mousePos[0]}px`);
-    document.body.style.setProperty(
-      "--y",
-      `${window.pageYOffset + mousePos[1]}px`
-    );
-  };
-
-  const onTouch = (ev: TouchEvent) => {
-    setTorchPos(ev.touches[0]);
-  };
-
-  document.body.addEventListener("mousemove", setTorchPos, elo);
-  document.addEventListener("scroll", updateTorchStyle, elo);
-
-  document.body.addEventListener("touchmove", onTouch, elo);
-  document.body.addEventListener("touchstart", onTouch, elo);
-  document.body.addEventListener(
-    "touchend",
-    () => {
-      setTorchPos({
-        clientX: -window.innerWidth,
-        clientY: -window.innerHeight,
-      });
-    },
-    elo
-  );
-}
+new MultiLanguageRouter({
+  languages: new Set(["de", "en"]),
+  defaultLanguage: navigator.language.includes("de") ? "de" : "en",
+  frame: new DomFrame("#root"),
+  sitemap: new Set(["portfolio", "info", "links"]),
+  fallbackSite: "404",
+  homeSite: "portfolio",
+  setWindowTitle: (newPage: string) => "Frank Mayer - " + capitalize(newPage),
+});
