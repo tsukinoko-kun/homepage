850de59a5f880de0be78bc16a8b81bdcfa073f66
Frank Mayer <frank.mayer1907@protonmail.com>
2022-03-16 22:47:59 +0100

    cleanup

--- src/script/router.ts
+++ src/script/router.ts
@@ -1,23 +1,14 @@
-import { addSlideInObserver, cancelSlideInObserver } from "./contentSlideIn";
-import { insertEMail } from "./mail";
 import { setPageNameAsBodyClass } from "./setPageNameAsBodyClass";
 import type { RoutedEvent } from "photon-re";
 
 export const routerEl = document.getElementById("root")!;
 
 routerEl.addEventListener("route", () => {
-  cancelSlideInObserver();
   routerEl.classList.add("loading");
 });
 
 routerEl.addEventListener("routed", (ev) => {
   setPageNameAsBodyClass((ev as RoutedEvent).detail.route);
-
-  for (const el of Array.from(routerEl.children)) {
-    addSlideInObserver(el);
-  }
-
-  insertEMail();
 
   setTimeout(() => {
     routerEl.classList.remove("loading");
