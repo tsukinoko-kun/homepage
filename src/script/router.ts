37b14638c5bc7fb0b94ca8d227bd14604ef7aad5
Frank Mayer <frank.mayer1907@protonmail.com>
2022-05-07 11:19:02 +0200

    added splash

--- src/script/router.ts
+++ src/script/router.ts
@@ -1,6 +1,6 @@
 import { setPageNameAsBodyClass } from "./setPageNameAsBodyClass";
 import type { RoutedEvent } from "photon-re";
-import path from "@frank-mayer/magic/Path";
+import { join } from "@frank-mayer/magic";
 
 export const routerEl = document.getElementById("root")!;
 
@@ -30,8 +30,35 @@ setTimeout(() => {
       ?.querySelectorAll("a[data-route]")
       .forEach((el) => {
         const a = el as HTMLAnchorElement;
-        const newHref = "/" + path.join("en/en", a.dataset.route!);
+        const newHref = "/" + join("en/en", a.dataset.route!);
         a.dataset.route = a.href = newHref;
       });
   }
 }, 1000);
+
+const splashEl = document.getElementById("splash");
+if (splashEl) {
+  Promise.all(
+    (
+      Array.from(
+        document.querySelectorAll('img:not([loading="lazy"])')
+      ) as Array<HTMLImageElement>
+    ).map(
+      (el) =>
+        new Promise<void>((resolve) => {
+          if (el.complete && el.naturalHeight !== 0) {
+            resolve();
+          } else {
+            el.addEventListener("load", () => resolve());
+          }
+        })
+    )
+  ).then(() => {
+    splashEl.remove();
+  });
+
+  // Fallback
+  setTimeout(() => {
+    splashEl.remove();
+  }, 5000);
+}
