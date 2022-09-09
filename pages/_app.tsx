3c6d00f244a3a76396ad795d9ee72c9af858e701
Frank-Mayer <mail@frank-mayer.io>
2022-09-09 23:08:18 +0200

    page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,15 +4,40 @@ import "../styles/mobile.scss";
 import type { AppProps } from "next/app";
 import { Nav } from "../components/Nav";
 import { Footer } from "../components/Footer";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
-const MyApp = (props: AppProps) => (
-  <>
-    <Nav />
-    <main>
-      <props.Component {...props.pageProps} />
-    </main>
-    <Footer />
-  </>
-);
+const MyApp = (props: AppProps) => {
+  const router = useRouter();
+  return (
+    <AnimatePresence mode="wait">
+      <Nav />
+      <motion.main
+        key={router.route}
+        initial="initialState"
+        animate="animateState"
+        exit="exitState"
+        transition={{ duration: 0.2 }}
+        variants={{
+          initialState: {
+            opacity: 0,
+            y: 100,
+          },
+          animateState: {
+            opacity: 1,
+            y: 0,
+          },
+          exitState: {
+            opacity: 0,
+            y: -100,
+          },
+        }}
+      >
+        <props.Component {...props.pageProps} />
+      </motion.main>
+      <Footer />
+    </AnimatePresence>
+  );
+};
 
 export default MyApp;
