5dedb416bf2cca96faf0236085a02403f7d7b6ed
Frank-Mayer <mail@frank-mayer.io>
2022-09-09 02:06:23 +0200

    auto sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://www.frank-mayer.io",
+  generateRobotsTxt: true,
+  autoLastmod: true,
+  trailingSlash: false,
+  changefreq: "weekly",
+};
