16d698dd178c74cfef2321cbd784cee721814a5f
Frank-Mayer <mail@frank-mayer.io>
2022-11-02 20:33:13 +0100

    added path to 404 message

--- src/routes/[...all]/index.tsx
+++ src/routes/[...all]/index.tsx
@@ -1,6 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import { PrimaryContainer } from "../../components/container";
-import { Link, RequestHandler } from "@builder.io/qwik-city";
+import { Link, type RequestHandler, useLocation } from "@builder.io/qwik-city";
 
 export const onGet: RequestHandler<{}> = (request) => {
   const redirectRegex = /^\/(de|en)\/(home|portfolio|contact)/g;
@@ -9,26 +9,36 @@ export const onGet: RequestHandler<{}> = (request) => {
   if (match) {
     request.response.status = 301;
     request.response.headers.set("Location", `/${match[2]}`);
+  } else {
+    request.response.status = 404;
   }
 };
 
-export default component$(() => (
-  <>
-    <PrimaryContainer background="rect" title="404" description="Not found" />
-    <h2>This is not the page you are looking for.</h2>
-    <p>Sorry, we couldn't find the page you were looking for.</p>
-    <p>
-      Try the{" "}
-      <Link
-        style={{
-          color: "var(--color-primary)",
-          textDecoration: "underline",
-        }}
-        href="/"
-      >
-        Home page
-      </Link>
-      ?
-    </p>
-  </>
-));
+export default component$(() => {
+  const loc = useLocation();
+
+  return (
+    <>
+      <PrimaryContainer
+        background="rect"
+        title="404"
+        description={`Path "${loc.pathname}" not found`}
+      />
+      <h2>This is not the page you are looking for.</h2>
+      <p>Sorry, we couldn't find the page you were looking for.</p>
+      <p>
+        Try the{" "}
+        <Link
+          style={{
+            color: "var(--color-primary)",
+            textDecoration: "underline",
+          }}
+          href="/"
+        >
+          Home page
+        </Link>
+        ?
+      </p>
+    </>
+  );
+});
