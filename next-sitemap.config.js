effed2ceab7e15f1a3478468b988b8d9ae612700
Frank-Mayer <mail@frank-mayer.io>
2022-11-02 01:29:25 +0100

    qwik

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://www.frank-mayer.io",
-  generateRobotsTxt: true,
-  autoLastmod: true,
-  trailingSlash: false,
-  changefreq: "weekly",
-};
---
+++ src/components/header/links.ts
@@ -0,0 +1,9 @@
+import { Logo } from "../icons/logo";
+import { Forum } from "../icons/forum";
+import { Code } from "../icons/code";
+
+export const links = [
+  { href: "/", icon: Logo, label: "Home" },
+  { href: "/portfolio", icon: Code, label: "Portfolio" },
+  { href: "/contact", icon: Forum, label: "Contact" },
+];
