9d297dc4b579391ff66e98afb8f4a344100eb848
Frank-Mayer <mail@frank-mayer.io>
2022-11-23 15:58:18 +0100

    AdSense

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 const Document = () => {
   return (
     <Html lang="en-US">
-      <Head />
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1590375905385922"
+          crossOrigin="anonymous"
+        ></script>
+      </Head>
       <body>
         <Main />
         <NextScript />
