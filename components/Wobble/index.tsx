e08558a3f81761792ba2cf5131f2784a31f72c5e
Frank-Mayer <mail@frank-mayer.io>
2022-12-23 18:19:30 +0100

    better wobble animation

--- components/Wobble/index.tsx
+++ components/Wobble/index.tsx
@@ -5,36 +5,46 @@ type Props = {
     children: string;
 }
 
-const wobbleAnimation = [
-    { transform: "scale(1)", transformOrigin: "50% 50%" },
-    { transform: "scale(0.7, 0.3)" },
-    { transform: "scale(0.975, 0.9)" },
-    { transform: "scale(0.875, 0.5)" },
-    { transform: "scale(0.9875, 0.95)" },
-    { transform: "scale(0.9375, 0.75)" },
-    { transform: "scale(1)", transformOrigin: "50% 50%" },
-]
+const getWobbleAnimation = (char: string) => {
+    const charIsTall = isTall(char)
+
+    return [
+        { transform: "scale(1)", transformOrigin: charIsTall ? "50% 50%" : "50% 60%" },
+        { transform: "scale(0.7, 0.1)" },
+        { transform: "scale(0.975, 0.9)" },
+        { transform: "scale(0.875, 0.4)" },
+        { transform: "scale(0.9875, 0.95)" },
+        { transform: "scale(0.9375, 0.5)" },
+        { transform: "scale(1)", transformOrigin: charIsTall ? "50% 50%" : "50% 60%" },
+    ]
+}
+
+const isTall = (char: string) => /^[A-Z0-9ltfi]$/.test(char)
 
 export const Wobble = (props: Props) => (
     <>
-        {props.children.split("").map((char, index) => (
-            char === " "
-                ? char
-                : <span className={styles.wobble} key={index} onMouseOver={(ev) => {
-                    if (client.isTouchDevice) {
-                        return
-                    }
+        {props.children.split("").map((char, index) => {
+            if (char == " ") {
+                return " "
+            }
 
-                    const target = ev.target as HTMLElement
+            const wobbleAnimation = getWobbleAnimation(char)
 
-                    if (target.getAnimations().length != 0) {
-                        return
-                    }
+            return <span className={styles.wobble} key={index} onMouseOver={(ev) => {
+                if (client.isTouchDevice) {
+                    return
+                }
 
-                    target.animate(wobbleAnimation, {
-                        duration: 750,
-                    })
-                }}>{char}</span>
-        ))}
+                const target = ev.target as HTMLElement
+
+                if (target.getAnimations().length != 0) {
+                    return
+                }
+
+                target.animate(wobbleAnimation, {
+                    duration: 750,
+                })
+            } }>{char}</span>
+        })}
     </>
 )
