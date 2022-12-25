54cbb3e6678b9a055fc6633c86b187c733e79926
Frank-Mayer <mail@frank-mayer.io>
2022-12-25 20:10:21 +0100

    space theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import "../styles/index.scss"
 import { motion, AnimatePresence } from "framer-motion"
 import type { AppProps } from "next/app"
@@ -9,6 +10,7 @@ import CaveatFont from "../fonts/Caveat.woff2"
 import JetBrainsMonoFont from "../fonts/JetBrainsMono.woff2"
 import CircularSpBookFont from "../fonts/CircularSp-Book.woff2"
 import { LoadFonts } from "../components/LoadFonts"
+import { useEffect } from "react"
 
 const variants = {
     hidden: { opacity: 0, x: -100, y: 0 },
@@ -16,8 +18,44 @@ const variants = {
     exit: { opacity: 0, x: 100, y: 0 },
 }
 
+const getScrollY = () =>
+    ((document.documentElement.scrollTop || document.body.scrollTop) /
+        ((document.documentElement.scrollHeight || document.body.scrollHeight) -
+            document.documentElement.clientHeight))
+
+const getSizeY = () => document.documentElement.scrollHeight || document.body.scrollHeight
+
+const onWindowScroll = () => {
+    window.document.documentElement.style.setProperty(
+        "--scroll-y",
+        getScrollY().toString()
+    )
+
+    window.document.documentElement.style.setProperty(
+        "--size-y",
+        getSizeY() + "px"
+    )
+}
+
+const scrollAELOptions = { passive: true }
+
 export default function App({ Component, pageProps }: AppProps) {
     const { title, description } = pageProps
+
+    useEffect(() => {
+        if (!window || (window as any)["__app_effect__"]) {
+            return
+        }
+
+        (window as any)["__app_effect__"] = true
+
+        window.addEventListener("scroll", onWindowScroll, scrollAELOptions)
+
+        return () => {
+            (window as any)["__app_effect__"] = false
+            window.removeEventListener("scroll", onWindowScroll)
+        }
+    })
 
     return (
         <>
@@ -25,7 +63,13 @@ export default function App({ Component, pageProps }: AppProps) {
                 <title>{`Frank Mayer \\\\ ${title}`}</title>
                 <meta name="description" content={description} />
             </Head>
-            <LoadFonts fonts={[{ name:"Caveat", url:CaveatFont }, { name:"JetBrainsMono", url: JetBrainsMonoFont }, { name:"CircularSp", url: CircularSpBookFont }]} />
+            <LoadFonts
+                fonts={[
+                    { name: "Caveat", url: CaveatFont },
+                    { name: "JetBrainsMono", url: JetBrainsMonoFont },
+                    { name: "CircularSp", url: CircularSpBookFont },
+                ]}
+            />
             <XmlTag tag="html" attributes={{ lang: "en-US" }} />
             <XmlTag tag="head">
                 <XmlTag tag="title">{`Frank Mayer \\\\ ${title}`}</XmlTag>
@@ -48,7 +92,7 @@ export default function App({ Component, pageProps }: AppProps) {
                         <Component {...pageProps} />
                     </motion.main>
                 </AnimatePresence>
-                <Footer/>
+                <Footer />
                 <XmlTag tag="script" language="jsx">
                     {`import React from "react"
 import { createRoot } from "react-dom/client"
