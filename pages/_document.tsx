4be414c2bb94d2b6b0cd025d6c5aaa238fce9ba9
Frank Mayer <mail@frank-mayer.io>
2022-12-26 01:14:03 +0100

    use next/script for google adsense code

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,15 +3,17 @@ import CaveatFont from "../fonts/Caveat.woff2"
 import JetBrainsMonoFont from "../fonts/JetBrainsMono.woff2"
 import CircularSpBookFont from "../fonts/CircularSp-Book.woff2"
 import { PreLoadFonts } from "../components/LoadFonts"
+import Script from "next/script"
 
 const Document = () => (
     <Html lang="en-US">
         <Head>
-            <script
+            <Script
                 async
+                data-ad-client="ca-pub-1590375905385922"
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1590375905385922"
                 crossOrigin="anonymous"
-            ></script>
+            />
             <PreLoadFonts fonts={[{ name:"Caveat", url:CaveatFont }, { name:"JetBrainsMono", url: JetBrainsMonoFont }, { name:"CircularSp", url: CircularSpBookFont }]} />
         </Head>
         <body>
