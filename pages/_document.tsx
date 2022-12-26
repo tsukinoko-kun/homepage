c583b870cc46a3c5d3bff67ccdb42dc65a86f30b
Frank Mayer <mail@frank-mayer.io>
2022-12-26 01:21:50 +0100

    fixed googlesyndication script tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,18 +3,17 @@ import CaveatFont from "../fonts/Caveat.woff2"
 import JetBrainsMonoFont from "../fonts/JetBrainsMono.woff2"
 import CircularSpBookFont from "../fonts/CircularSp-Book.woff2"
 import { PreLoadFonts } from "../components/LoadFonts"
-import Script from "next/script"
 
 const Document = () => (
     <Html lang="en-US">
         <Head>
-            <Script
+            <PreLoadFonts fonts={[{ name:"Caveat", url:CaveatFont }, { name:"JetBrainsMono", url: JetBrainsMonoFont }, { name:"CircularSp", url: CircularSpBookFont }]} />
+            <script
                 async
                 data-ad-client="ca-pub-1590375905385922"
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1590375905385922"
                 crossOrigin="anonymous"
             />
-            <PreLoadFonts fonts={[{ name:"Caveat", url:CaveatFont }, { name:"JetBrainsMono", url: JetBrainsMonoFont }, { name:"CircularSp", url: CircularSpBookFont }]} />
         </Head>
         <body>
             <Main />
@@ -23,7 +22,7 @@ const Document = () => (
                 dangerouslySetInnerHTML={{
                     __html: "navigator.serviceWorker.getRegistrations().then(function(registrations) {for(const registration of registrations) {registration.unregister()}})",
                 }}
-            ></script>
+            />
         </body>
     </Html>
 )
