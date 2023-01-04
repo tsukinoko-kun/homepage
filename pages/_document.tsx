cf14ee0055e7b6ae6e1a507ab336db7e29884585
Frank Mayer <mail@frank-mayer.io>
2023-01-04 14:03:49 +0100

    removed google adsense

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,12 +3,6 @@ import { Html, Head, Main, NextScript } from "next/document"
 const Document = () => (
     <Html lang="en-US">
         <Head>
-            <script
-                async
-                data-ad-client="ca-pub-1590375905385922"
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1590375905385922"
-                crossOrigin="anonymous"
-            />
             <meta property="og:site_name" content="Frank Mayer" />
             <meta property="og:type" content="website" />
             <meta name="twitter:site" content="@FF1493FF" />
