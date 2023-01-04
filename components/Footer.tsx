a8d66bd8302ba824121b6f7c76bf51d72f71dd71
Frank Mayer <mail@frank-mayer.io>
2023-01-04 13:01:38 +0100

    added space above footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,28 +1,31 @@
 import { XmlTag } from "./XmlTag"
 
 export const Footer = () => (
-    <XmlTag tag="footer">
-        <XmlTag tag="table">
-            <XmlTag tag="tbody">
-                <XmlTag tag="tr">
-                    <table cellSpacing={0}>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <XmlTag tag="td">
-                                        <XmlTag tag="a" href="https://github.com/Frank-Mayer">GitHub</XmlTag>
-                                    </XmlTag>
-                                </td>
-                                <td>
-                                    <XmlTag tag="td">
-                                        <XmlTag tag="a" href="https://www.linkedin.com/in/frank-mayer-b85677214">LinkedIn</XmlTag>
-                                    </XmlTag>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+    <>
+        <br />
+        <XmlTag tag="footer">
+            <XmlTag tag="table">
+                <XmlTag tag="tbody">
+                    <XmlTag tag="tr">
+                        <table cellSpacing={0}>
+                            <tbody>
+                                <tr>
+                                    <td>
+                                        <XmlTag tag="td">
+                                            <XmlTag tag="a" href="https://github.com/Frank-Mayer">GitHub</XmlTag>
+                                        </XmlTag>
+                                    </td>
+                                    <td>
+                                        <XmlTag tag="td">
+                                            <XmlTag tag="a" href="https://www.linkedin.com/in/frank-mayer-b85677214">LinkedIn</XmlTag>
+                                        </XmlTag>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </XmlTag>
                 </XmlTag>
             </XmlTag>
         </XmlTag>
-    </XmlTag>
+    </>
 )
