8d2d625c849a42afc15a14cf0ebdeccd12cfc712
Frank Mayer <frank.mayer1907@protonmail.com>
2023-01-25 01:01:42 +0100

    astro framework rework (#233)

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.SITE_URL || "https://www.frank-mayer.io",
-  generateRobotsTxt: true,
-  autoLastmod: true,
-  changefreq: "monthly",
-  trailingSlash: false,
-};
