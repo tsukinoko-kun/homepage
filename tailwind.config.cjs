44b6a58724217801760e666626c43618a461c54f
Frank Mayer <frank.mayer1907@protonmail.com>
2023-10-22 19:21:45 +0200

    Tailwind (#368)

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        colors: {
+            background: "rgb(var(--color-background) / <alpha-value>)",
+            foreground: "rgb(var(--color-foreground) / <alpha-value>)",
+            primary: "rgb(var(--color-primary) / <alpha-value>)",
+            inactive: "rgb(var(--color-inactive) / <alpha-value>)",
+            selection: "rgb(var(--color-selection) / <alpha-value>)",
+        },
+    },
+    plugins: [],
+};
