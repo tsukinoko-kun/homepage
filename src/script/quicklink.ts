5b0992a9b89078ce768d61a42cb649920fdc19d3
Frank Mayer <gh@frankmayer.dev>
2024-04-04 19:03:05 +0200

    fixed quicklink

--- src/script/quicklink.ts
+++ src/script/quicklink.ts
@@ -1,22 +1,3 @@
 import { listen } from "quicklink";
 
-let quicklingStarted = false;
-
-function startQuickling() {
-    if (quicklingStarted) {
-        return;
-    }
-    quicklingStarted = true;
-    listen();
-}
-
-switch (document.readyState) {
-    case "interactive":
-    case "complete":
-        listen();
-        break;
-    default:
-        window.addEventListener("load", startQuickling);
-        window.setTimeout(startQuickling, 5000);
-        break;
-}
+listen();
