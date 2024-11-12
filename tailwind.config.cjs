dba4daf440c33fdd6abb85c14388dd469aff2f4a
Frank Mayer <gh@frankmayer.dev>
2024-11-12 17:54:45 +0100

    fix stuff

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,15 +2,22 @@
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        colors: {
-            background: "rgb(var(--color-background) / <alpha-value>)",
-            foreground: "rgb(var(--color-foreground) / <alpha-value>)",
-            primary: "rgb(var(--color-primary) / <alpha-value>)",
-            secondary: "rgb(var(--color-secondary) / <alpha-value>)",
-            inactive: "rgb(var(--color-inactive) / <alpha-value>)",
-            disabled: "rgb(var(--color-disabled) / <alpha-value>)",
-            selection: "rgb(var(--color-selection) / <alpha-value>)",
+        extend: {
+            colors: {
+                background: "rgb(var(--color-background) / <alpha-value>)",
+                foreground: "rgb(var(--color-foreground) / <alpha-value>)",
+                primary: "rgb(var(--color-primary) / <alpha-value>)",
+                secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+                inactive: "rgb(var(--color-inactive) / <alpha-value>)",
+                disabled: "rgb(var(--color-disabled) / <alpha-value>)",
+                selection: "rgb(var(--color-selection) / <alpha-value>)",
+            },
         },
     },
-    plugins: [],
+    plugins: [
+        require("@catppuccin/tailwindcss")({
+            prefix: "ctp",
+            defaultFlavour: "latte",
+        }),
+    ],
 };
