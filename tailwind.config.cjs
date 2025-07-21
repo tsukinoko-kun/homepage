1abbe65bd8750e18e0c87b4e0113673fe5862237
Frank Mayer <gh@frankmayer.dev>
2025-07-21 17:12:22 +0200

    started vcs stuff

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,27 @@
+const coderFonts = [
+    "Monolisa",
+    "Gintronic",
+    "Dank Mono",
+    "PragmataPro",
+    "JetBrains Mono",
+    "Cascadia Code",
+    "Source Code Pro",
+    "Fira Code",
+    "DejaVu Sans Mono",
+    "Hack",
+    "Anonymous Pro",
+    "Ubuntu Mono",
+];
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
+        fontFamily: {
+            sans: [...coderFonts, "sans-serif"],
+            serif: [...coderFonts, "serif"],
+            mono: [...coderFonts, "monospace"],
+        },
         extend: {
             colors: {
                 background: "rgb(var(--color-background) / <alpha-value>)",
